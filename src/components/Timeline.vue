cbcf5c29a74b4eb2dc68ca2427e1d3a95371fd21
David Petter <cfdavidpetter@gmail.com>
2019-10-27 19:30:52 -0300

    Adding multiple cards.

--- src/components/Timeline.vue
+++ src/components/Timeline.vue
@@ -1,14 +1,18 @@
 <template>
   <div>
 
-    <div class="row">
+    <div class="row" v-for="card in cards" v-bind:key="card.id">
       <div class="col-3 line">
-        <img class="card rounded float-right" src="https://www.greendot.com/for-people/our-products/prepaid-cards/prepaid-mastercard-or-visa-card/_jcr_content/root/responsivegrid/responsivegrid/product_detail_hero/hero_img--desktop.coreimg.png/1567537206489.png">
+        <img class="card rounded float-right" src="https://i.ibb.co/gdgVQwg/card-1.png">
       </div>
       <div class="col-9 card-description">
         <p class="text-left">
-          <b>Status:</b> Active<br>
-          <b>Send:</b> 03/18/2019<br>
+          <span class="number">
+            <b>Number:</b> <b-badge variant="secondary">{{card.number}}</b-badge><br>
+          </span>
+          <b>Card Category:</b> {{card.category}}<br>
+          <b>Status:</b> {{card.status}}<br>
+          <b>Send:</b> {{card.send}}<br>
         </p>
       </div>
     </div>
@@ -20,6 +24,30 @@
 
 export default {
   name: 'Timeline',
+  data: function () {
+    return {
+      cards: {
+        0:{
+          number: 434233,
+          category: 'Bronze',
+          status: 'Active',
+          send: '03/18/2019'
+        },
+        1:{
+          number: 234001,
+          category: 'Silver',
+          status: 'Disabled',
+          send: '03/01/2016'
+        },
+        2:{
+          number: 123233,
+          category: 'Gold',
+          status: 'Disabled',
+          send: '11/20/2012'
+        },
+      }
+    }
+  }
 }
 </script>
 
@@ -29,7 +57,7 @@ export default {
   border-right-style: dotted;
 }
 .card{
-  width: 130px;
+  width: 115px;
   margin-top: 20px;
   margin-right: -65px;
   margin-bottom: 20px;
@@ -40,6 +68,10 @@ export default {
   align-items: center;
 }
 .card-description p {
-  margin-left: 50px;
+  margin-left: 39px;
+  margin-bottom: 0px;
+}
+.number {
+  font-size: 20px;
 }
 </style>
